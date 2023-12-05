2d362a4bfc52eb1821b52b3541bf4c4a82c0cca6
Garrick Ngai <garrickdark@yahoo.com>
2023-12-05 05:31:46 -0800

    initialized repo

---
+++ MedicalRecordSystem.java
@@ -0,0 +1,9 @@
+public class MedicalRecordSystem {
+    // Placeholder attributes and methods
+    // These will be expanded upon in subsequent commits
+
+    public static void main(String[] args) {
+        // Placeholder main method
+        System.out.println("Medical Record System");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# CIS-256-Final-Medical-Record-System
+# Medical Record System
+
+This repository contains the source code for the Medical Record System project.
+
+## Usage
+
+To run the program, execute the following command:
+
+```bash
+java MedicalRecordSystem
