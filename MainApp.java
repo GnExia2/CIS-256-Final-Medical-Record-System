26fb59c5748416c9b30577718a32ec195ebfd6eb
Garrick Ngai <garrickdark@yahoo.com>
2023-12-07 14:18:48 -0800

    updated adding patients

--- MainApp.java
+++ MainApp.java
@@ -1,19 +1,35 @@
-// MainApp.java
-
 public class MainApp {
     public static void main(String[] args) {
         // Create an instance of MedicalRecordSystem
         MedicalRecordSystem medicalRecordSystem = new MedicalRecordSystem();
 
-        // Create a Graph instance
-        Graph graph = new Graph();
+        // Example: Creating and adding a patient
+        Patient newPatient1 = new Patient("P001", "John Doe", 30, "Hypertension", 2);
+        medicalRecordSystem.addPatient(newPatient1);
 
-        // Example: Creating a Patient
-        Patient newPatient = new Patient("P001", "John Doe", 30, "Hypertension", 2);
+        Patient newPatient2 = new Patient("P002", "Jane Smith", 25, "Asthma", 3);
+        medicalRecordSystem.addPatient(newPatient2);
 
-        // Example: Adding the patient to the system using PatientManager
-        PatientManager patientManager = new PatientManager(graph);
-        patientManager.addPatient(medicalRecordSystem, newPatient);
+        Patient newPatient3 = new Patient("P003", "Bob Johnson", 40, "Diabetes", 1);
+        medicalRecordSystem.addPatient(newPatient3);
+
+        Patient newPatient4 = new Patient("P004", "Alice Brown", 22, "Migraine", 2);
+        medicalRecordSystem.addPatient(newPatient4);
+
+        Patient newPatient5 = new Patient("P005", "Tom Wilson", 35, "Arthritis", 4);
+        medicalRecordSystem.addPatient(newPatient5);
+
+        Patient newPatient6 = new Patient("P006", "Sara Davis", 28, "Allergies", 1);
+        medicalRecordSystem.addPatient(newPatient6);
+
+        Patient newPatient7 = new Patient("P007", "Mike Miller", 45, "Heart Disease", 3);
+        medicalRecordSystem.addPatient(newPatient7);
+
+        Patient newPatient8 = new Patient("P008", "Emily White", 33, "Depression", 2);
+        medicalRecordSystem.addPatient(newPatient8);
+
+        Patient newPatient9 = new Patient("P009", "Chris Robinson", 50, "Cancer", 5);
+        medicalRecordSystem.addPatient(newPatient9);
 
         // Run the user interface
         MedicalRecordSystemUI.runUserInterface(medicalRecordSystem);
--- MedicalRecordSystem.java
+++ MedicalRecordSystem.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Set;
 
 public class MedicalRecordSystem {
@@ -12,6 +11,7 @@ public class MedicalRecordSystem {
     private PriorityQueue<Appointment> appointmentQueue;
     private BTree medicalRecordsBTree;
     private List<Patient> allPatients;
+    private PriorityQueue<Patient> patientPriorityQueue;
     
 
     // Constructor to initialize the MedicalRecordSystem
@@ -32,19 +32,16 @@ public class MedicalRecordSystem {
         graph.addEdge(patient1, patient2);
     }
 
-    // Method to prioritize patients based on medical history length
-    public void prioritizePatients() {
-        Queue<Patient> priorityQueue = new PriorityQueue<>((p1, p2) ->
-                Integer.compare(p2.getMedicalHistory().length(), p1.getMedicalHistory().length()));
+    // Method to prioritize patients
+    public List<Patient> prioritizePatients() {
+        List<Patient> prioritizedPatients = new ArrayList<>();
 
-        for (Patient patient : graph.getAllPatients()) {
-            priorityQueue.add(patient);
+        // Extract patients from the priority queue
+        while (!patientPriorityQueue.isEmpty()) {
+            prioritizedPatients.add(patientPriorityQueue.poll());
         }
 
-        System.out.println("Prioritized Patients:");
-        while (!priorityQueue.isEmpty()) {
-            displayPatientInformation(priorityQueue.poll());
-        }
+        return prioritizedPatients;
     }
 
     // Method to optimize treatment plans for all patients
@@ -79,16 +76,9 @@ public class MedicalRecordSystem {
 
     // Method to get all appointments
     public List<Appointment> getAllAppointments() {
-        List<Appointment> allAppointments = new ArrayList<>();
-
-        while (!appointmentQueue.isEmpty()) {
-            allAppointments.add(appointmentQueue.poll());
-        }
-
+        List<Appointment> allAppointments = new ArrayList<>(appointmentQueue);
         return allAppointments;
     }
-
-
 
     // Getter methods for data structures
     public Set<String> getMedicalConditionsSet() {
